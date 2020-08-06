f3906287ea7e693b276c1458ce9700611139af7b
bloop <jaroslavkalinin97@gmail.com>
2020-08-06 23:03:10 +0300

    Users model added

--- src/server/app.js
+++ src/server/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var signupRouter = require('./routes/signup');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -20,7 +21,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/signup', signupRouter);
+app.use('/login', loginRouter);
+
+
+//delete soon
+app.get('/test', (req, res) => {
+  res.json({message: 'pass'});
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,13 +37,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({message: err.message});
 });
 
 module.exports = app;
